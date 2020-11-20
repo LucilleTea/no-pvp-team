78c0e09c487a708fabd94fdb5d9f9377e926cef0
Lucy-t <64509677+Lucy-t@users.noreply.github.com>
2020-11-20 22:55:59 +1030

    cleanup imports, add todo

--- src/main/java/net/lucilletea/noPvpTeam/mixin/MixinBucketItem.java
+++ src/main/java/net/lucilletea/noPvpTeam/mixin/MixinBucketItem.java
@@ -1,28 +1,14 @@
 package net.lucilletea.noPvpTeam.mixin;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BucketItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.packet.s2c.play.BlockEventS2CPacket;
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.InventoryS2CPacket;
-import net.minecraft.screen.slot.Slot;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.tag.FluidTags;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.core.jmx.Server;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -39,6 +25,7 @@ public class MixinBucketItem {
     @Shadow @Final private Fluid fluid;
 
     // Prevent bucket placement if it's in or near another player, and at least one of you has pvp off.
+    // TODO: sync this to the client more nicely
     @Inject(method = "placeFluid(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/hit/BlockHitResult;)Z",
     at = @At(value = "HEAD"), cancellable = true)
     private void mixinPlaceFluid(@Nullable PlayerEntity player, World world, BlockPos pos, @Nullable BlockHitResult blockHitResult, CallbackInfoReturnable<Boolean> cir) {
